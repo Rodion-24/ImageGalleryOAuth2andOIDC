2a357a8242ffd9fb83e752f9d98938d6aee6ad54
Rodion-24 <your-email@example.com>
2025-11-22 23:42:54 +0100

    add auth policy by contry

--- ImageGallery.Client/Program.cs
+++ ImageGallery.Client/Program.cs
@@ -1,3 +1,4 @@
+using ImageGallery.Authorization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
@@ -56,7 +57,9 @@ builder.Services.AddAuthentication(options =>
     options.ClaimActions.DeleteClaim("idp");
     options.Scope.Add("roles");
     options.Scope.Add("imagegalleryapi.fullaccess");
+    options.Scope.Add("country");
     options.ClaimActions.MapJsonKey("role", "role");
+    options.ClaimActions.MapUniqueJsonKey("country", "country");
     options.SaveTokens = true;
     options.GetClaimsFromUserInfoEndpoint = true;
     options.TokenValidationParameters = new()
@@ -65,6 +68,9 @@ builder.Services.AddAuthentication(options =>
         RoleClaimType = "role",
     };
 });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("UserCanAddImage", AuthorizationPolicies.CanAddImage());
 
 var app = builder.Build();
 
--- Rodion.IDP/Config.cs
+++ Rodion.IDP/Config.cs
@@ -10,7 +10,10 @@ public static class Config
         {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
-            new IdentityResource("roles", "You role(s)", new [] { "role" })
+            new IdentityResource("roles", "You role(s)", new [] { "role" }),
+            new IdentityResource("country",
+                "The country you're living in",
+                new List<string>(){"country"})
         };
 
     public static IEnumerable<ApiResource> ApiResources =>
@@ -50,7 +53,8 @@ public static class Config
                     IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.Profile,
                     "roles",
-                    "imagegalleryapi.fullaccess"
+                    "imagegalleryapi.fullaccess",
+                    "country"
                 },
                 ClientSecrets = { new Secret("secret".Sha256()) },
                 RequireConsent = true
